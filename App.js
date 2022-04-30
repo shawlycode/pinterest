d09285914b0b255a3f29b3de3135013a61abc1b0
Shawlycode <kutinaorlando@gmail.com>
2022-04-30 19:08:17 +0000

    committed

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import React, { useEffect } from "react";
 
 import Router from "./src/Navigations/Route";
+// import { Provider } from "react-redux";
+// import { createStore } from "redux";
 
+// const store = createStore(MyShopTusReducer);
 export default function App() {
   return <Router />;
 }
